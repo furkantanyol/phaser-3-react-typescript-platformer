3a20a1653cacd229b89304dccfe11f8e3db73c53
Furkan Tanyol <furkantanyol@gmail.com>
2020-03-21 11:21:42 +1100

    Updates

---
+++ precache-manifest.fa68596d529005ddd20eef7a9cddd906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3c573c956f8e49d5a9a4607b39bf91",
+    "url": "/phaser-3-react-typescript-platformer/index.html"
+  },
+  {
+    "revision": "265a40b8bddfae543cdf",
+    "url": "/phaser-3-react-typescript-platformer/static/css/main.00503b4e.chunk.css"
+  },
+  {
+    "revision": "cc59628ab7c625f3873e",
+    "url": "/phaser-3-react-typescript-platformer/static/js/2.cc353f70.chunk.js"
+  },
+  {
+    "revision": "0e84afde8a138d28ddc413c2c1367a0a",
+    "url": "/phaser-3-react-typescript-platformer/static/js/2.cc353f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265a40b8bddfae543cdf",
+    "url": "/phaser-3-react-typescript-platformer/static/js/main.9b181d23.chunk.js"
+  },
+  {
+    "revision": "dd4754fccf8d58d79ab5",
+    "url": "/phaser-3-react-typescript-platformer/static/js/runtime-main.78d4735d.js"
+  }
+]);
